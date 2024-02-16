3cbf895649659df17a002bd8d00f41994445f79d
iCode-Content <114425302+iCode-Content@users.noreply.github.com>
2024-02-16 14:23:28 -0600

    Moved files into Sprint_1 Class_6

---
+++ Class_6/Function_x.java
@@ -0,0 +1,11 @@
+public class Function_x {
+    public static void main(String[] args){
+     
+        x("Carter");
+    }
+
+    public static void x(String name){
+        System.out.println("Hello " + name);
+    }
+}
+
